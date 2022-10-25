05ab3c62570c0209b242f6291374c4296498beae
Evgenii Zhmero-Gaidenko <106560016+jmuriki@users.noreply.github.com>
2022-10-25 15:58:49 +0300

    Put together the imports

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,12 +1,6 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
-from django.shortcuts import render
-
-from django.shortcuts import get_object_or_404
-
-from datacenter.models import get_duration
-from datacenter.models import format_duration
-from datacenter.models import is_visit_long
+from datacenter.models import Passcard, Visit
+from django.shortcuts import render, get_object_or_404
+from datacenter.models import get_duration, format_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,10 +1,6 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Passcard, Visit
 from django.shortcuts import render
-
-from datacenter.models import get_duration
-from datacenter.models import format_duration
-from datacenter.models import is_visit_long
+from datacenter.models import get_duration, format_duration, is_visit_long
 
 
 def storage_information_view(request):
